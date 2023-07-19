2f573fa300786a85570c7a777c7f6f42ccd69853
avalcd00 <avalcd00@estudiantes.unileon.es>
2023-07-19 14:07:49 +0200

    Ciclos de una Activity

--- app/src/main/java/com/example/hello_world/MainActivity.kt
+++ app/src/main/java/com/example/hello_world/MainActivity.kt
@@ -2,10 +2,52 @@ package com.example.hello_world
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    val tag = "MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val helloText = findViewById<TextView>(R.id.hello_text)
+        helloText.text = "Hello Android"
+
+        Log.d(tag, "onCreate")
     }
+
+    override fun onStart() {
+        Log.d(tag, "onStart")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        Log.d(tag, "onResume")
+        super.onResume()
+    }
+
+    override fun onPause() {
+        Log.d(tag, "onPause")
+        super.onPause()
+    }
+
+    override fun onStop() {
+        Log.d(tag, "onStop")
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        Log.d(tag, "onDestroy")
+        super.onDestroy()
+    }
+
+
+
+
+
+
+
+
 }
