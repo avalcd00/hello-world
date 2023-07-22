c6678ae2bfadbeeaa707f5808a6686603b24ac28
avalcd00 <avalcd00@estudiantes.unileon.es>
2023-07-22 17:33:29 +0200

    SetOnClickListener Added

--- app/src/main/java/com/example/hello_world/MainActivity.kt
+++ app/src/main/java/com/example/hello_world/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.hello_world
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.webkit.WebView.FindListener
+import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -15,9 +18,19 @@ class MainActivity : AppCompatActivity() {
         /*val helloText = findViewById<TextView>(R.id.hello_text)
         helloText.text = "Hello Android"*/
 
-        Log.d(tag, "onCreate")
-    }
+        val email = findViewById<TextView>(R.id.email)
+        val phone = findViewById<TextView>(R.id.phone)
+        val sendButton = findViewById<Button>(R.id.send)
 
+        sendButton.setOnClickListener(){
+            val message = "Email: ${email.text}, Teléfono: ${phone.text}"
+            Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+            Log.d(tag, "Button Clicked")
+        }
+
+        //Log.d(tag, "onCreate")
+    }
+/*
     override fun onStart() {
         Log.d(tag, "onStart")
         super.onStart()
@@ -44,7 +57,7 @@ class MainActivity : AppCompatActivity() {
     }
 
 
-
+*/
 
 
 
